ac3e61d56abf5f61b53127d4efe5b938370a5651
Astrid <scarlettstrid@gmail.com>
2022-05-24 18:03:10 +0800

    Able to crawl from steam

--- crawler.py
+++ crawler.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 class Format:
     def __init__(self, format):
-        self.redirect_url = re.compile(format['RE_redirectURL'])
+        # self.redirect_url = re.compile(format['RE_redirectURL'])
         # self.priceFormat = re.compile(format['RE_priceFormat'])
         self.not_found = re.compile(format['RE_notFound'])
         self.element = re.compile(format['RE_element'])
@@ -17,14 +17,20 @@ class Format:
         data = []
         if(len(self.not_found.findall(html)) == 0):
             elements = self.element.findall(html)
-            print(len(elements))
+            if(len(elements) > 10):
+                elements = elements[:10]
             for el in elements:
                 info = {}
-                info['price'] = self.price.findall(el)[0]
+                info['price'] = self.price.findall(el)
+                if(len(info['price']) == 0):
+                    info['price'] = '-'
+                else:
+                    info['price'] = info['price'][0]
                 info['title'] = self.title.findall(el)[0]
                 info['image'] = self.image.findall(el)[0]
                 data.append(info)
         return data
+
     redirect_url = ''
     priceFormat = ''
     not_found = ''
@@ -44,17 +50,19 @@ class Web:
 def toSearchURL(toSearch,web):
     searchInput = web['format']['urlSpace'].join(toSearch.split(' '))
     url = web['format']['requestURL'].split(' ')
-    url = ''.join([url[0],searchInput,url[1]])
+    if(len(url)==2):
+        url = ''.join([url[0],searchInput,url[1]])
+    else:
+        url = web['format']['requestURL']+searchInput
     return url
 
 def getHTML(url, browser):
     browser.get(url)
-    html = browser.page_source
-    browser.quit()
+    html = browser.page_source.replace('\n','')
     return html
 
 def search(toSearch):
-    results = []
+    results = {}
     driverPath = 'chromedriver.exe'
     browser = webdriver.Chrome(driverPath)
     # browser.set_window_position(-10000,0)
@@ -62,10 +70,14 @@ def search(toSearch):
     for web in webs:
         url = toSearchURL(toSearch,web)
         html = getHTML(url, browser)
-
         format = Format(web['format'])
         data = format.findInfos(html)
-        print(data)
-        pass
+        results[web['name']]=data
+        print(web['name']+" DONE!!!")
+    return results
 
-search('overcooked')
+results = search('apex')
+for r in results:
+    print(r+" =================================================")
+    for rr in results[r]:
+        print(rr['title']+" - "+rr['price']+" - "+rr['image'])
--- db.py
+++ db.py
@@ -7,12 +7,26 @@ webs = [
         "format":{
             "requestURL" : 'https://store.epicgames.com/en-US/browse?q= &sortBy=relevancy&sortDir=DESC&count=40',
             "urlSpace": '%20',
-            "RE_redirectURL" : '<a class=\"css-1jx3eyg\".+</a>',
             "RE_element":'(<li class=\"css-lrwy1y\".*?</li>)',
             "RE_price" : '<span class=\"css-z3vg5b\" data-component=\"Text\">(NT\$[\d|,|\.]+|Free)</span>',
             "RE_image" : '<img alt=\".+\" src=\"([^\"]+)\"',
             "RE_title" : '<div data-testid=\"direction-auto\" class=\"css-1h2ruwl\" data-component=\"DirectionAuto\">([^<]+)',
             "RE_notFound": '<span data-component=\"Message\">No results found</span>',
+        }
+    },
+    {
+        "name":'Steam',
+        "url":'https://store.steampowered.com/',
+        "currency":'NTD',
+        "toNTD":1,
+        "format":{
+            "requestURL" : 'https://store.steampowered.com/search/?term=',
+            "urlSpace": '+',
+            "RE_element":'(<a.*?search_result_row.*?</a>)',
+            "RE_price" : '(Free To Play|Free|NT\$ [\d.,]+)[^(</strike>)]*?</div>',
+            "RE_image" : '<img src=\"(.*?)\".*?>',
+            "RE_title" : '<span class=\"title\">(.*?)</span>',
+            "RE_notFound": '0 results match your search',
         }
     },
 ]
