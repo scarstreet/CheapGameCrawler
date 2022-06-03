cc83d7cb50c02580a9844ebd3ef2e837ab6d4774
Astrid <scarlettstrid@gmail.com>
2022-06-03 18:51:10 +0800

    Cleaned code + added descriptive comments

--- CrawlyBoi.py
+++ CrawlyBoi.py
@@ -1,8 +1,12 @@
+import crawler as cr
 import tkinter as tk
 from tkinter import ttk
-import crawler as cr
 from PIL import ImageTk, Image
 
+'''======================================================
+A class to define the scrollable frame
+when displaying the search results on the app
+======================================================'''
 class ScrollableFrame(ttk.Frame):
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
@@ -24,6 +28,57 @@ class ScrollableFrame(ttk.Frame):
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
 
+'''======================================================
+Some runtime functions that are activated
+when the search button is pressed
+======================================================'''
+
+# Creating the UI for displaying each data
+def changeContent(data):
+    global content_frame
+    global allImg
+    content_frame.destroy()
+    content_frame = ScrollableFrame(root)
+    allImg = []
+    for d in data:
+        if(len(data[d])!=0):
+            data_frame = tk.Frame(content_frame.scrollable_frame)
+            title = tk.Label(data_frame,text=d,font=("Impact", 18),bg="#e3631e",fg="#ffffff")
+            title.pack(side="top",fill="x")
+            for idx,row in enumerate(data[d].to_dict('records')):
+                color = "#e5e5e5" if idx%2 == 0 else "#d4d4d4"
+                info_frame = tk.Label(data_frame,bg=color)
+                print(row)
+                try:
+                    allImg.append('.')
+                    allImg[-1] = Image.open(row['imgPath'])
+                    w,h = allImg[-1].size
+                    if h >= 50:
+                        ratio = h//50
+                        print(allImg[-1].size,(w//ratio,h//ratio))
+                        allImg[-1] = allImg[-1].resize((w//ratio,h//ratio),Image.ANTIALIAS)
+                    allImg[-1] = ImageTk.PhotoImage(allImg[-1])
+                    tk.Label(info_frame,image=allImg[-1]).pack(side="left")
+                except:
+                    tk.Label(info_frame,bg="#ffffff",text="ImageError!",width=10,height=5,anchor="center").pack(side="left")
+                tk.Label(info_frame,text=row['title'],font=("Helvatica", 12),bg=color).pack(side="left", expand=True, fill='x')
+                tk.Label(info_frame,text=f"NT$ {row['price']}",font=("Impact", 14),bg=color).pack(side="right")
+                info_frame.pack(side="top", fill='x', expand=True)
+            data_frame.pack(side="top", fill='x')
+    content_frame.pack(expand=True, fill="both",side="bottom",anchor="center")
+
+# Start searching the results by connecting to the
+# crawler file. After obtaining results, call the
+# above function
+def startSearch():
+    all = cr.search(search.get())
+    changeContent(all)
+
+
+
+'''======================================================
+GUI setup for the app's general information
+======================================================'''
 global root
 root = tk.Tk()
 root.title("Crawly Boi")
@@ -39,64 +94,11 @@ center_y = int(screen_height/2 - window_height / 2) - 40
 
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-# root.resizable(False, False)
+'''======================================================
+GUI setup for the app's contents
+======================================================'''
 
-# TODO - get an icon!!
-# root.iconbitmap('./assets/pythontutorial.ico')
-# SETUP^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# FUNCTIONS vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 allImg = []
-
-def changeContent(data):
-    global content_frame
-    global allImg
-    content_frame.destroy()
-    content_frame = ScrollableFrame(root)
-    allImg = []
-    for d in data:
-        if(len(data[d])!=0):
-            data_frame = tk.Frame(content_frame.scrollable_frame)
-            title = tk.Label(data_frame,text=d,font=("Impact", 18),bg="#e3631e",fg="#ffffff")
-            title.pack(side="top",fill="x")
-            for idx,row in enumerate(data[d].to_dict('records')):
-                # while(True):
-                # try:
-                color = "#e5e5e5" if idx%2 == 0 else "#d4d4d4"
-                info_frame = tk.Label(data_frame,bg=color)
-                print(row)
-                # image = ImageTk.PhotoImage(Image.open(row['imgPath']).resize((200,200),Image.ANTIALIAS))
-                try:
-                    allImg.append('.')
-                    allImg[-1] = Image.open(row['imgPath'])
-                    w,h = allImg[-1].size
-                    if h >= 50:
-                        ratio = h//50
-                        print(allImg[-1].size,(w//ratio,h//ratio))
-                        allImg[-1] = allImg[-1].resize((w//ratio,h//ratio),Image.ANTIALIAS)
-                    allImg[-1] = ImageTk.PhotoImage(allImg[-1])
-                    # canv = tk.Canvas(info_frame,width=200, height=200)
-                    # canv.pack(side="left")
-                    tk.Label(info_frame,image=allImg[-1]).pack(side="left")
-                except:
-                    tk.Label(info_frame,bg="#ffffff",text="ImageError!",width=10,height=5,anchor="center").pack(side="left")
-                # canv.create_image(20,20,image=image)
-                tk.Label(info_frame,text=row['title'],font=("Helvatica", 12),bg=color).pack(side="left", expand=True, fill='x')
-                tk.Label(info_frame,text=f"NT$ {row['price']}",font=("Impact", 14),bg=color).pack(side="right")
-                info_frame.pack(side="top", fill='x', expand=True)
-                # except Exception as e:
-                #     print(e)
-            # ttk.Separator(data_frame,orient="horizontal").pack()
-            data_frame.pack(side="top", fill='x')
-    # placeholdr = tk.Label(text="I tried", bg="#b3b3b3")
-    # placeholdr.pack(expand=True, fill="both",side="bottom")
-    content_frame.pack(expand=True, fill="both",side="bottom",anchor="center")
-
-def button_clicked():
-    all = cr.search(search.get())
-    changeContent(all)
-    print(all)
-
-# GUI vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 mainTitle = tk.Label(root, text="CRAWLY BOI",font=("Impact", 30))
 mainTitle.pack(fill='x', side="top")
@@ -111,7 +113,7 @@ search = tk.StringVar()
 search_entry = ttk.Entry(searchFrame, textvariable=search)
 search_entry.pack(side=tk.LEFT, expand=True, fill='x', padx=10,pady=20)
 
-button = ttk.Button(searchFrame, text='Search', command=button_clicked)
+button = ttk.Button(searchFrame, text='Search', command=startSearch)
 button.pack(side=tk.LEFT, padx=10,pady=20)
 
 global content_frame
@@ -120,5 +122,7 @@ placeholdr = tk.Label(content_frame,text="Note : This app takes a while to load.
 placeholdr.pack(expand=True, fill="both",side="bottom")
 content_frame.pack(expand=True, fill="both",side="bottom")
 
-# RUN vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+'''======================================================
+Starting the app
+======================================================'''
 root.mainloop()
--- crawler.py
+++ crawler.py
@@ -1,30 +1,56 @@
-import requests, re
-import time
+# db is the database of websites. Each containing:
+# - name of the store
+# - url of the store's webstire
+# - currency of the store & its conversion to NTD
+# - regex strings to crawl each data
 from db import webs
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-import pandas as pd
 
+import requests, re
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver.chrome.options import Options
+
+'''======================================================
+A class to define the regex formats of what will
+need to be crawled from each website and a function
+to extract the crawled items of the website
+======================================================'''
 class Format:
+    '''======================================================
+    Functions
+    ======================================================'''
     def __init__(self, format):
+        # init related to crawling
         self.not_found = re.compile(format['RE_notFound'])
         self.element = re.compile(format['RE_element'])
         self.price = re.compile(format['RE_price'])
         self.image = re.compile(format['RE_image'])
         self.title = re.compile(format['RE_title'])
         
+        # init related to currency
         self.currency = format['currency']
         self.toNTD = format['toNTD']
 
     def findInfos(self,html):
         data = []
+        # making sure there is a search result in the web befor
+        # crawling. sometimes some webs don't have some games
         if(len(self.not_found.findall(html)) != 1):
+            # finding the data element of the web. Containing
+            # the info of each game's price, title and image
             elements = self.element.findall(html)
+
+            # Limiting the search result for each web to 10
+            # to not overload the app & not waste time on
+            # irrelevant results
             if(len(elements) > 10):
                 elements = elements[:10]
+            
+            # Crawling data from each element
             for el in elements:
                 info = {}
+                # Price ========================================================
                 info['price'] = self.price.findall(el)
                 if(len(info['price']) == 0):
                     info['price'] = '0'
@@ -35,11 +61,20 @@ class Format:
                     info['price'] = self.numbersOnly.findall(info['price'])[0]
                     if(self.currency != 'NTD'):
                         info['price'] = f"{(float(info['price'].replace(',',''))*self.toNTD):.2f}"
+
+                # Title ========================================================
                 info['title'] = self.title.findall(el)[0]
+
+                # Image ========================================================
+                # the src link to the image
                 info['image'] = self.image.findall(el)[0]
+
+                # appending the information to a list that will be returned later on
                 data.append(info)
         return data
-
+    '''======================================================
+    Class Data
+    ======================================================'''
     redirect_url = ''
     priceFormat = ''
     not_found = ''
@@ -52,6 +87,33 @@ class Format:
     toNTD = ''
     numbersOnly = re.compile('[\d\.,]+')
 
+'''======================================================
+Functions to do the crawling process
+======================================================'''
+# Main search function
+def search(toSearch):
+    results = {}
+    driverPath = 'chromedriver.exe'
+    options = Options()
+    browser = webdriver.Chrome(driverPath, options=options)
+    browser.set_window_position(-10000,0)
+
+    for web in webs:
+        # crawling from each website in the database
+        url = toSearchURL(toSearch,web)
+        html = getHTML(url, browser)
+        format = Format(web['format'])
+        data = format.findInfos(html)
+        results[web['name']]=data
+    for result in results:
+        # Turning existing results to data frames and downloading
+        # Their pictures
+        if(len(results[result]) > 0):
+            results[result] = toDF(results[result])
+            results[result] = downloadImages(results[result], result)
+    return results
+
+# Function to generate the url used to instantly search
 def toSearchURL(toSearch,web):
     searchInput = web['format']['urlSpace'].join(toSearch.split(' '))
     url = web['format']['requestURL'].split(' ')
@@ -61,31 +123,14 @@ def toSearchURL(toSearch,web):
         url = web['format']['requestURL']+searchInput
     return url
 
+# getting the searched url's source html
 def getHTML(url, browser):
     browser.get(url)
     html = browser.page_source.replace('\n','')
     return html
 
-def search(toSearch):
-    results = {}
-    driverPath = 'chromedriver.exe'
-    options = Options()
-    # options.headless = True
-    browser = webdriver.Chrome(driverPath, options=options)
-    browser.set_window_position(-10000,0)
-
-    for web in webs:
-        url = toSearchURL(toSearch,web)
-        html = getHTML(url, browser)
-        format = Format(web['format'])
-        data = format.findInfos(html)
-        results[web['name']]=data
-    for result in results:
-        if(len(results[result]) > 0):
-            results[result] = toDF(results[result])
-            results[result] = downloadImages(results[result], result)
-    return results
-
+# Converting the search result to a dataframe instead of a dictionary
+# along with sorting the search result by price
 def toDF(dict):
     df = pd.DataFrame(dict)
     df.loc[df["price"] == "", "price"] = '0'
@@ -99,6 +144,9 @@ def toDF(dict):
     df = df.reset_index()
     return df
 
+# downloading the images based on the dataframe
+# and adding the image's path for each data to the
+# data frame
 def downloadImages(df, name):
     data = []
     for id,d in enumerate(df['image'].values.tolist()):
@@ -106,12 +154,12 @@ def downloadImages(df, name):
         namee = name.replace('.', '')
         end = ".png"
         if("GOG" in namee):
+            # an error occured when using GOG it's not solved yet
+            # and idk what's causing it. But since it's not important
+            # for the topic, I shall let it be 
             end = ".jpg"
         with open(f'images\\{namee}-{id}{end}', 'wb') as handler:
             handler.write(img_data)
         data.append(f'images/{namee}-{id}{end}')
     df['imgPath'] = data
     return df
-
-# results = search('red dead')
-# print(results)
